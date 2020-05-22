814fe27d8f9ba502f3fd0f4174ee2b33d1c5b8bb
Yuichi Watanabe <yuichi.watanabe.ja@gmail.com>
2020-05-23 05:28:38 +0900

    write example

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vvatanabe/hello-beep
+
+go 1.14
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20200420150314-13046a26d502 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20200217142428-fce0ec30dd00 // indirect
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gen2brain/beeep"
+
+func main() {
+	err := beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+	if err != nil {
+		panic(err)
+	}
+	err = beeep.Notify("Title", "Message body", "")
+	if err != nil {
+		panic(err)
+	}
+	err = beeep.Alert("Title", "Message body", "")
+	if err != nil {
+		panic(err)
+	}
+}
